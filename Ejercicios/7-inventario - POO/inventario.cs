7f522bb1a653ca778f3def4257cbfb28ca4ac986
Jose Eduardo Gonzales <jegonzalesr@unah.hn>
2022-07-13 20:00:24 -0600

    inventario foreach video#18

--- Ejercicios/7-inventario - POO/inventario.cs
+++ Ejercicios/7-inventario - POO/inventario.cs
@@ -1,20 +1,35 @@
 using System;
+using System.Collections.Generic;
 public class Inventario{
 
-      string[,] productos = new string[5,3]
-        {
-            {"001","Iphone X",       "0"},
-            {"002","Laptop Dell",    "5"},
-            {"003","Monitor Samsung","2"},
-            {"004","Mouse",          "100"},
-            {"005","Headset",        "25"},
+    //   string[,] productos = new string[5,3]
+    //     {
+    //         {"001","Iphone X",       "0"},
+    //         {"002","Laptop Dell",    "5"},
+    //         {"003","Monitor Samsung","2"},
+    //         {"004","Mouse",          "100"},
+    //         {"005","Headset",        "25"},
            
-        };
+    //     };
+
+    public List<Producto> ListadeProductos { get; set; }
 
             public Inventario()
             {
-                
-        }
+                ListadeProductos = new List<Producto>();
+
+                Producto a = new Producto ("001", "iphoneX", 0);
+                Producto b = new Producto ("002", "Laptop Dell", 5);
+                Producto c = new Producto ("003", "Monitor Samsung", 2);
+                Producto d = new Producto ("004", "Mouse", 100);
+                Producto e = new Producto ("005", "Headset", 25);
+
+                ListadeProductos.Add(a);
+                ListadeProductos.Add(b);
+                ListadeProductos.Add(c);
+                ListadeProductos.Add(d);
+                ListadeProductos.Add(e);
+            }
 
           public void listarProductos(){
             Console.Clear();
@@ -23,24 +38,31 @@ public class Inventario{
             Console.WriteLine("*********************");
             Console.WriteLine("Codigo , Descripcion , y Existencia");
 
-            for (int i = 0; i < 5; i++)
+            // for (int i = 0; i < 5; i++)
+            // {
+            //     Console.WriteLine(productos[i , 0] + " | " + productos[i , 1] + " | " + productos[i , 2]); 
+            // }
+            foreach (var producto in ListadeProductos)
             {
-                Console.WriteLine(productos[i , 0] + " | " + productos[i , 1] + " | " + productos[i , 2]); 
+                Console.WriteLine(producto.Codigo + " | " + producto.Descripcion + " | " + producto.Existencia.ToString());
             }
              Console.ReadLine();
         }
+        
            
         public void movimientoInventario(string codigo, int cantidad, string tipoMovimiento) {
-        for (int i = 0; i < 5; i++)
-        {
-            if (productos[i , 0] == codigo) {
+    
+
+       foreach (var producto in ListadeProductos)
+       {
+            if (producto.Codigo == codigo) {
                 if (tipoMovimiento == "+") {
-                    productos[i , 2] = (Int32.Parse(productos[i , 2]) + cantidad).ToString();
+                    producto.Existencia = producto.Existencia + cantidad;
                 } else {
-                    productos[i , 2] = (Int32.Parse(productos[i , 2]) - cantidad).ToString();
+                    producto.Existencia = producto.Existencia - cantidad;
                 }
             }
-        }
+       } 
     }
 
         public void ingresoDeInventario() {
